c5c54a03cba70b45d4376dfda467488ac0b1a7dc
Ben Burns <37713740+laconic1@users.noreply.github.com>
2022-08-19 18:48:39 -0500

    uploading so can continue on other computer

---
+++ Data/PizzaService.cs
@@ -0,0 +1,17 @@
+//using dapper
+
+namespace BlazingPizza.Data;
+
+public class PizzaService
+{
+    // public Task<Pizza[]> GetPizzasAsync()
+    // {
+    //     // Call your data access technology here
+
+    //     var connString = "Data Source=.;Initial Catalog=DATABASE_NAME;Integrated Security=True;";
+
+    //     using var connection = new SqlConnection(connString);
+
+    //     var a = connection.Query()
+    // }
+}
